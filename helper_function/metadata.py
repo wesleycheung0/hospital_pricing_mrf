e5f8b19bd5aa9ab0aa25682f89c28dd926a10327
wesleycheung0 <wesley6d05@gmail.com>
2024-01-12 23:49:21 -0500

    Add billing code

--- helper_function/metadata.py
+++ helper_function/metadata.py
@@ -1,2 +1,13 @@
 billing_category = ['professional','facility']
 setting_category = ['inpatient','outpatient','both']
+billing_code = [
+    "ms_drg",
+    "apr_drg",
+    "eapg",
+    "hcpcs_cpt",
+    "modifiers",
+    "alt_hcpcs_cpt",
+    "apc",
+    "icd",
+    "ndc"
+]
